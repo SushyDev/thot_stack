1fc053d4148843546134d018e7f52380cfd8f7f1
SushyDev <xsushy@hotmail.com>
2023-08-13 15:37:22 +0200

    add picture of jay diesel

--- lib/pages/home.ml
+++ lib/pages/home.ml
@@ -1,9 +1,12 @@
 open Tyxml.Html
 
 let heading_title = "caml my ocaml deez!"
+let image_src = "/static/jay_diesel.jpg"
 
+(* Not sure if this should be a function or a variable *)
 let render = [
   main [
-    h1 [ txt heading_title ]
+    h1 [ txt heading_title ];
+    img ~src:image_src ~alt:"jay diesel" ~a:[a_width 420] ();
   ]
 ]
