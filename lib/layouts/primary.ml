d539037382a6cb78e089cfd45349d794885cdbe0
SushyDev <xsushy@hotmail.com>
2023-08-13 21:47:17 +0200

    bundle htmx

--- lib/layouts/primary.ml
+++ lib/layouts/primary.ml
@@ -1,6 +1,5 @@
 open Tyxml.Html
 let page_title = "Caml layout"
-let src_htmx = "https://unpkg.com/htmx.org@1.9.4"
 
 let render (elements) =
 	let head_elt = head (title (txt page_title)) [
@@ -8,7 +7,6 @@ let render (elements) =
 		(meta ~a:[a_name "description"; a_content "Caml layout"] ());
 		(link ~rel:[`Stylesheet] ~href:"/dist/style.css" ());
 		(script ~a:[a_src "/dist/index.js"] (txt ""));
-		(script ~a:[a_src src_htmx] (txt ""));
 	] in
 
 	html ~a:[a_lang "en"] (head_elt) (body elements)
