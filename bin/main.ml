ddba9c22b9687f60845e8a383d1ffb6bce58ffc9
SushyDev <xsushy@hotmail.com>
2023-08-14 23:50:16 +0200

    some more newbie code

--- bin/main.ml
+++ bin/main.ml
@@ -5,8 +5,21 @@ let doc_to_string (page) =
   let parser = Fmt.str "%a" formatter in 
   parser page
 
+(* let elt_to_string (elt) = *)
+(*   let formatter = Tyxml.Html.pp_elt () in *)
+(*   let parser = Fmt.str "%a" formatter in  *)
+(*   parser elt *)
+
+let lwt_elt_to_string elt =
+  let open Lwt.Syntax in
+  let formatter = Tyxml.Html.pp_elt () in
+  let parser = Fmt.str "%a" formatter in 
+  let* elt = elt in
+  Dream.html @@ parser @@ elt
+
 let () = Dream.run 
   ~port
+  @@ Dream.logger
   @@ Dream.router [
     Dream.get "/dist/**" @@ Dream.static "./_build/dist";
     Dream.get "/" (fun _ ->
@@ -14,5 +27,23 @@ let () = Dream.run
       @@ doc_to_string
       @@ Layouts.Primary.render
       @@ Pages.Home.render
+    );
+    Dream.get "/about" (fun _ ->
+      Dream.html
+      @@ doc_to_string
+      @@ Layouts.Primary.render
+      @@ Pages.About.render
+    );
+    Dream.get "/todo" (fun _ ->
+      Dream.html
+      @@ doc_to_string
+      @@ Layouts.Primary.render
+      @@ Pages.Todo.render
+    );
+
+    (* api *)
+    Dream.put "/api/todo-add" (fun request -> 
+      lwt_elt_to_string
+      @@ Pages.Todo.item(request)
     );
   ]
---
+++ lib/pages/todo.ml
@@ -0,0 +1,29 @@
+open Tyxml.Html
+
+let heading_title = "todo camls"
+
+(* Not sure if this should be a function or a variable *)
+let render = [
+  main [
+    h1 ~a:[a_class ["text-green-500"; "text-xl"]] [ txt "Todo List" ];
+    form ~a:[
+      Unsafe.string_attrib "hx-put" "/api/todo-add";
+      Unsafe.string_attrib "hx-target" "#todo-list";
+      Unsafe.string_attrib "hx-swap" "beforeend";
+      Unsafe.string_attrib "hx-on::after-request" "this.reset()";
+    ] [
+        input ~a:[a_input_type `Text; a_placeholder "todo item"; a_name "todo_item"; a_class ["border"; "border-black"]] ();
+        input ~a:[a_input_type `Submit; a_value "Add"; a_class ["border"; "border-black"]] ();
+      ];
+    ul ~a:[a_id "todo-list"] [];
+  ];
+]
+
+
+let item request =
+  let open Lwt.Syntax in
+  let* body = Dream.body request in
+  let stuff = Dream.from_form_urlencoded body in 
+  let todo_item = List.assoc "todo_item" stuff in
+
+  Lwt.return (li [ txt (todo_item) ])
